7bca444ddc60b6058e453d1db3a47d62a671991a
AlyShmahell <aly.shmahell@gmail.com>
2020-04-02 13:01:49 +0200

    added camera component prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'camera', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'camera', loadChildren: () => import('./camera/camera.module').then( m => m.CameraPageModule) },
 ];
 
 @NgModule({
